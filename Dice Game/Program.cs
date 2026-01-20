656a277b59a42d263ed4cf23705cfeffcaf82f57
Ahmeddouhm <a.douha0@gmail.com>
2026-01-21 01:06:46 +0200

    Add Simple Dice Game

---
+++ Dice Game/Program.cs
@@ -0,0 +1,91 @@
+﻿Console.WriteLine("==========================");
+Console.WriteLine("Hello To Dice Game :)");
+Console.WriteLine("To Exit Enter \"ex\"");
+
+Console.Write("Enter Your Name : ");
+string username = Console.ReadLine();
+
+bool isFinished = false;
+int rounds;
+
+Random rnd = new Random();
+
+while (!isFinished)
+{
+    int scoreUser = 0;
+    int scoreAI = 0;
+    Console.WriteLine("==========================");
+    Console.Write("Enter Rounds : ");
+    string? input = Console.ReadLine();
+    if (input == "ex")
+    {
+        break;
+    }
+    else 
+    {
+        rounds = int.Parse(input);
+    }
+
+    if (!(rounds % 2 == 1) && rounds > 0)
+    {
+        Console.WriteLine("Enter a Valid Odd Number >)");
+        continue;
+    }
+
+    for (int i = 0; i < rounds; i++)
+    {
+        int diceUser = rnd.Next(1, 7);
+        int diceAI = rnd.Next(1, 7);
+
+        Console.WriteLine("Press Any Key To Roll");
+        Console.ReadKey();
+        Console.WriteLine($"-- Round {i+1} --");
+        Console.Write(".");
+        Thread.Sleep(500);
+        Console.Write(".");
+        Thread.Sleep(500);
+        Console.Write(".\n");
+        Thread.Sleep(500);
+
+        if (diceUser > diceAI)
+        {
+            scoreUser++;
+            Console.WriteLine($"{username} Won => {scoreUser} : {scoreAI}");
+            Console.WriteLine($"{username} Won => {diceUser} : {diceAI}");
+        }
+        else if (diceUser < diceAI)
+        {
+            scoreAI++;
+            Console.WriteLine($"AI Won => {scoreUser} : {scoreAI}");
+            Console.WriteLine($"AI Won => {diceUser} : {diceAI}");
+        }
+        else 
+        {
+            Console.WriteLine("DRAW");
+            i--;
+            continue;
+        }
+
+        int roundsToWin = (int)Math.Ceiling((double)rounds / 2);
+        int remainRounds = rounds - (i + 1);
+
+        if (scoreUser == roundsToWin)
+        {
+            Console.WriteLine($"{username} Won't Lose Today hahaha");
+        }
+        else if (scoreAI == roundsToWin) 
+        {
+            Console.WriteLine("AI Won't Lose Today hahaha");
+        }
+
+
+        if (scoreAI + remainRounds < roundsToWin)
+        {
+            Console.WriteLine($"Congratulations in advance {username}");
+        } else if (scoreUser + remainRounds < roundsToWin) 
+        {
+            Console.WriteLine("Congratulations in advance AI");
+        }
+        
+    }
+}

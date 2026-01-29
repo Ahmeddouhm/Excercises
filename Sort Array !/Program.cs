9faea90168f29d9869f4738870764de478a5f7d6
Ahmeddouhm <a.douha0@gmail.com>
2026-01-29 05:59:29 +0200

    Add Func to creata fully randomized arrays

---
+++ Sort Array !/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine("=======================");
+Console.WriteLine("Welcome To Sort Array !");
+Console.WriteLine("=======================");
+
+Console.Write("Enter Array Size : ");
+int arrSize = int.Parse(Console.ReadLine());
+Console.Write("Enter Range Of Values : ");
+int arrRangeOfVals = int.Parse(Console.ReadLine());
+
+CreateArray(arrSize , arrRangeOfVals);
+
+
+static void CreateArray(int size , int range) 
+{
+    Random rnd = new Random();
+    int[] array = new int[size];
+    int[] userArray = new int[size];
+
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        int numToCheck;
+        do
+        {
+            numToCheck = rnd.Next(1, range);
+        } while (array.Contains(numToCheck));
+
+        array[i] = numToCheck;
+        Console.Write($"[{array[i]}]");
+    }
+}
